f5bcf0506c5831c5d3dd24a8d3c3b7f0deb9bd97
Mads Bo Mikkelsen <au615041@uni.au.dk>
2021-02-19 11:31:32 +0100

    test for git push - mads

--- Assignment1_Hotel/app.js
+++ Assignment1_Hotel/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// test from mads 
 var app = express();
 
 // view engine setup
