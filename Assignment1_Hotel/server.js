020b4397d3b883901b03d63a55a7d0d7f47321e0
Alexander <alexander@pop-os.localdomain>
2021-02-28 14:24:00 +0100

    Added main() with connection functionality

--- Assignment1_Hotel/server.js
+++ Assignment1_Hotel/server.js
@@ -1,6 +1,19 @@
 const mongoose = require('mongoose');
+
 let dbUrl = 'mongodb://localhost:27017/HotelFour';
+
 if (process.env.NODE_ENV === 'production') {
     dbUrl = process.env.MONGODB_URI;
 }
 
+async function main() {
+    try {
+        await mongoose.connect(dbUrl, {
+            useNewIrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+    } catch (error) {
+        console.log(error)
+    }
+}
