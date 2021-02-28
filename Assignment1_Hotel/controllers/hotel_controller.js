4d9b853e9ae0f06759afc577f4655099fd4e6825
Alexander <alexander@pop-os.localdomain>
2021-02-28 14:21:23 +0100

    Created 'addRoomToHotel'

--- Assignment1_Hotel/controllers/hotel_controller.js
+++ Assignment1_Hotel/controllers/hotel_controller.js
@@ -1,7 +1,32 @@
+const hotelCollection = require('../models/hotel');
+
 //
 //POST to create Hotel --Mads
 
 //POST to create rooms for Hotel-id -- Alex
+module.exports.addRoomToHotel = function (req, res) {
+    try {
+        let hotel = await hotelCollection.findByIdAndUpdate(req.params.id, {
+            roomNo: req.body.roomNo
+        }, {
+            new: true
+        });
+        if (hotel) // The hotel room was succesfully added to the hotel
+        res.status(200).json({
+            hotel
+        })
+        else {
+            res.status(500).json({
+                "title": "Unknown server error"
+            })
+        };
+    } catch (reason) {
+        res.status(400).json({
+            "title": "Unable to update hotel record",
+            "detail": reason
+        })
+    }   
+};
 
 // GET List of rooms from Hotel-id --Trang
 // -- list of available rooms from hotel-id - role = User
