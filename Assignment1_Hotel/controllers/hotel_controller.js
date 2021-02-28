25526f71c395ca4ad5a1b411707c123603bf5c94
Randi Mikkelsen Bech <au615785@uni.au.dk>
2021-02-28 15:18:18 +0100

    wip

--- Assignment1_Hotel/controllers/hotel_controller.js
+++ Assignment1_Hotel/controllers/hotel_controller.js
@@ -4,29 +4,6 @@ const hotelCollection = require('../models/hotel');
 //POST to create Hotel --Mads
 
 //POST to create rooms for Hotel-id -- Alex
-module.exports.addRoomToHotel = function (req, res) {
-    try {
-        let hotel = await hotelCollection.findByIdAndUpdate(req.params.id, {
-            roomNo: req.body.roomNo
-        }, {
-            new: true
-        });
-        if (hotel) // The hotel room was succesfully added to the hotel
-        res.status(200).json({
-            hotel
-        })
-        else {
-            res.status(500).json({
-                "title": "Unknown server error"
-            })
-        };
-    } catch (reason) {
-        res.status(400).json({
-            "title": "Unable to update hotel record",
-            "detail": reason
-        })
-    }   
-};
 
 // GET List of rooms from Hotel-id --Trang
 // -- list of available rooms from hotel-id - role = User
@@ -35,6 +12,17 @@ module.exports.addRoomToHotel = function (req, res) {
 //GET all rooms for All Hotels - role-type -- Randi
 // -- list of all my resevations -Role = guest
 //
+module.exports.getHotelsWithRooms = async function (req, res){
+    const hotels = await hotelsCollection.find({})
+        .catch(reason => res.status(400).json({
+            "title": "Error",
+            "detail": reason
+        }))
+    res.status(200).json({
+        hotels
+    })
+}
+
 
 //UPDATE list of resevations for Hotel-id -- role = HotelMangaer 
 
