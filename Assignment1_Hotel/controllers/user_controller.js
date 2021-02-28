ad4638140ef34e0fc6d30b9a96d1d38fa7f7ab40
Mads Bo Mikkelsen <au615041@uni.au.dk>
2021-02-28 20:00:52 +0100

    add getAllUsers and getUser

--- Assignment1_Hotel/controllers/user_controller.js
+++ Assignment1_Hotel/controllers/user_controller.js
@@ -1,11 +1,43 @@
-// 
+const userCollection = require('../models/user'); 
+
 
 // GET all users 
+module.exports.getAllUsers = async function (req, res ) {
+    const users = await userCollection.find({})
+        .catch(reason => 
+            res.status(400).json({
+                "title": "Unable to read users from the database",
+                "detail": reason
+            })
+        );
+    res.status(200).json({
+        users
+    }) 
+}; 
+
 // Get specific user from user-id
+module.exports.getUser = async function (req, res){
+    try{
+        const user = await userCollection.findById(req.params.id); 
+        if(user){
+            res.status(200).json({
+                user
+            })
+        } else {
+            throw ("User not found");
+        }
+    } catch (err) {
+        res.status(400).json({
+            "title": "Unable to read user from the database",
+            "detail": err
+        })
+    }; 
+}
+
 
 //UPDATE/PUT upgrade User to hotelMangaer by user-id
 
-// extra
+// 
 
 //- Create user
 //Delete User 
