9b2f327ce008e06b749faedc3b080626eeda599d
Alexander <alexsmith@live.dk>
2021-03-02 19:44:17 +0100

    added swagger for addRoomToHotel

--- Assignment1_Hotel/routes/hotels.js
+++ Assignment1_Hotel/routes/hotels.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const hotelController = require('../controllers/hotel_controller'); 
+const hotelController = require('../controllers/hotel_controller');
 
 /**
  * @swagger
@@ -31,16 +31,43 @@ const hotelController = require('../controllers/hotel_controller');
  *                         example: Alexander
  */
 
- router.route('')
+router.route('')
   .get(hotelController.index)
-  //.post(hotelController.addHotel)
+//.post(hotelController.addHotel)
 
-/* POST add hotel room */
+
+/**
+* @swagger
+* /hotels/:hotelid:
+*   post:
+*     summary: Add a room to a hotel
+*     description: Through a hotel id add a room to the hotel
+*     responses:
+*       201:
+*         description: Hotel room was created succesfully.
+*         content:
+*           application/json:
+*             schema:
+*               type: object
+*               properties:
+*                 data:
+*                   type: array
+*                   items:
+*                     type: object
+*                     properties:
+*                        hotelid: ObjectId
+*                        description: The hotels ID
+*                        example: 603e02dfcc68d8751453b861
+*                     room:
+*                        type: room
+*                        description: A json object describing a room
+*                        example: {"roomNo": 8,"reservations":[{"dateStart": "2021-03-02T09:18:23.807+00:00","guestId": "Randi"}]}
+*/
 router.route('/:hotelid')
-//.get(hotelController.getHotel)
-.put(hotelController.addRoomToHotel)
+  //.get(hotelController.getHotel)
+  .put(hotelController.addRoomToHotel)
 
 router.route('/AllHotelsWithRooms/:userId')
-.get(hotelController.getHotelsWithRooms);
+  .get(hotelController.getHotelsWithRooms);
 
 module.exports = router;
