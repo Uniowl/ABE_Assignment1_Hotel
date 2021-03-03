10f2dbfafa16f4bd6f94c14ae8c446ad51c4ad0f
Alexander <alexsmith@live.dk>
2021-03-03 14:59:45 +0100

    Removed redundant out-commented code

--- Assignment1_Hotel/models/user.js
+++ Assignment1_Hotel/models/user.js
@@ -41,22 +41,7 @@ const userSchema = new mongoose.Schema({
     },
     password: String,
     role: String
-}); 
-
-// FOR NOW THIS DOESN'T WORK:
-// - - - - - - - - - - - - - - -
-const jwt = require('express-jwt');
-userSchema.methods.generateJwt = function (){
-    let expiry = new Date();
-    expiry.setDate(expiry.getDate() + 7);
-
-    return jwt.sign({
-        sub: this._id,
-        email: this.email,
-        name: this.name,
-        exp: parseInt(expiry.getTime() / 1000) //unix time in seconds
-    }, process.env.JWT_SECRET);
-}
+});
 
 module.exports = mongoose.model('User', userSchema);
 
