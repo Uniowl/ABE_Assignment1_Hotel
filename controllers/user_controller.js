baebfce9863dcd4fa7e5c0eed6dbdccb417005a4
Mads Bo Mikkelsen <au615041@uni.au.dk>
2021-03-26 12:07:58 +0100

    swagger add auth

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,114 +1,121 @@
-const role = require('../helpers/role');
-const userCollection = require('../models/user'); 
+const role = require("../helpers/role");
+const userCollection = require("../models/user");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
-//register 
-module.exports.register = async function(req, res){
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
-        const user = await userCollection.create({
-            name: req.body.name,
-            password: hashedPassword,
-            role: role.User,
-            email: `${req.body.name}@hotelfour.dk`
+//register
+module.exports.register = async function (req, res) {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
+    const user = await userCollection.create({
+      name: req.body.name,
+      password: hashedPassword,
+      role: role.User,
+      email: `${req.body.name}@hotelfour.dk`,
+    });
+    res.send(user);
+  } catch (error) {
+    res.status(400).json({
+      title: "Unable to create student record",
+      detail: error,
+    });
+  }
+};
+
+//login
+const jwt = require("jsonwebtoken");
+const { HotelManager } = require("../helpers/role");
+module.exports.login = async function (req, res) {
+  try {
+    const user = await userCollection.findOne({ name: req.body.name });
+    console.log(user);
+    if (user) {
+      const compareResult = await bcrypt.compare(
+        req.body.password,
+        user.password
+      );
+      if (compareResult) {
+        const token = jwt.sign(
+          {
+            name: user.name,
+            email: user.email,
+            role: user.role,
+            // exp: parseInt(Date.now() / 1000 ) +60*60//unix time in seconds
+          },
+          process.env.JWT_SECRET
+        );
+        res.send({ token });
+      } else {
+        res.status(401).json({
+          message: "wrong username or password",
         });
-        res.send(user);
-    } catch (error) {
-        res.status(400).json({
-            "title": "Unable to create student record",
-            "detail": error
-        })
+      }
+    } else {
+      res.status(401).json({
+        message: "wrong username or password",
+      });
     }
-}
+  } catch (error) {
+    res.status(500).json({
+      message: "Unknown server error",
+    });
+  }
+};
 
-//login 
-const jwt = require('jsonwebtoken');
-const { HotelManager } = require('../helpers/role');
-module.exports.login = async function(req, res) {
-    try {
-        const user = await userCollection.findOne({name: req.body.name});
-        console.log(user);
-        if(user){
-            const compareResult = await bcrypt.compare(req.body.password, user.password);
-            if(compareResult){
-                const token = jwt.sign({
-                    name: user.name,
-                    email: user.email,
-                    role: user.role
-                   // exp: parseInt(Date.now() / 1000 ) +60*60//unix time in seconds
-                }, 
-                    process.env.JWT_SECRET
-                );
-                res.send({token});
-            } else {
-                res.status(401).json({
-                    message: "wrong username or password"
-                })
-            }
-        } else {
-            res.status(401).json({
-                message: "wrong username or password"
-            })
-        }
-    } catch (error) {
-        res.status(500).json({
-            message: "Unknown server error"
-        })
+// GET all users
+module.exports.getAllUsers = async function (req, res, next) {
+  try {
+    const users = await userCollection.find({});
+    if (users) {
+      res.status(200).json({ users });
+    } else {
+      throw "Users not found";
     }
-}
-
-// GET all users 
-module.exports.getAllUsers = async function (req, res, next ) {
-    try {
-        const users = await userCollection.find({}); 
-        if(users){
-            res.status(200).json({users})
-        } else {
-            throw ("Users not found"); 
-        }
-    } catch (error) {
-        res.status(400).json({
-            "title": "Unable to read users from the database",
-            "detail": error
-        })
-    }
-}; 
+  } catch (error) {
+    res.status(400).json({
+      title: "Unable to read users from the database",
+      detail: error,
+    });
+  }
+};
 
 // Get specific user from user-id
-module.exports.getUser = async function (req, res){
-    try{
-        const user = await userCollection.findById(req.params.userId); 
-        if(user){
-            res.status(200).json({
-                user
-            })
-        } else {
-            throw ("User not found");
-        }
-    } catch (err) {
-        res.status(400).json({
-            "title": "Unable to read user from the database",
-            "detail": err
-        })
-    }; 
-}
-
+module.exports.getUser = async function (req, res) {
+  try {
+    const user = await userCollection.findById(req.params.userId);
+    if (user) {
+      res.status(200).json({
+        user,
+      });
+    } else {
+      throw "User not found";
+    }
+  } catch (err) {
+    res.status(400).json({
+      title: "Unable to read user from the database",
+      detail: err,
+    });
+  }
+};
 
 //UPDATE/PUT upgrade User to hotelMangaer by user-id
-module.exports.upgradeUser = async function (req, res){
-    let userToUpgrade = await userCollection.findByIdAndUpdate(req.params.id,{
-        role: HotelManager
-    },{
-        new: true
-    })
-    if(userToUpgrade){
-        res.status(200).json({
-            userToChange: userToUpgrade
-        })
-    }else {
-        res.status(500).json({
-            "title":"Unknown server error"
-        })
-    };
-}
+module.exports.upgradeUser = async function (req, res) {
+  let userToUpgrade = await userCollection.findByIdAndUpdate(
+    req.params.id,
+    {
+      role: HotelManager,
+    },
+    {
+      new: true,
+    }
+  );
+  if (userToUpgrade) {
+    res.status(200).json({
+      userToChange: userToUpgrade,
+    });
+  } else {
+    res.status(500).json({
+      title: "Unknown server error",
+    });
+  }
+};
--- routes/hotels.js
+++ routes/hotels.js
@@ -1,8 +1,8 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const hotelController = require('../controllers/hotel_controller');
-const role = require('../helpers/role');
-const authorize = require('../helpers/authorize');
+const hotelController = require("../controllers/hotel_controller");
+const role = require("../helpers/role");
+const authorize = require("../helpers/authorize");
 
 /**
  * @swagger
@@ -32,22 +32,36 @@ const authorize = require('../helpers/authorize');
  *                         description: The name of the manager who runs the hotel.
  *                         example: Alexander
  */
-router.route('')
-  .get(hotelController.index);
+router.route("").get(hotelController.index);
 
 /**
  * @swagger
- * hotels/addHotel/:userId:
+ * hotels/addHotel/{userId}:
  *   post:
- *     security: 
+ *     security:
  *       - bearerAuth: JWT
  *     summary: Add new hotel to collection and assign user as manager
  *     description: Creates a new hotel and adds the given userId as a manager
- *     parameters: 
- *     - in: path
- *       name: userId
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         type: string
+ *         required: true
+ *         description: The unique ID of the user who will be assigned manager
+ *     requestBody:
  *       required: true
- *       description: The unique ID of the user who will be assigned manager
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 required: true
+ *                 description: name of hotel
+ *               rooms:
+ *                 type: object
+ *                 description: rooms at the hotel
  *     responses:
  *       200:
  *         description: The hotel was created and manager was added
@@ -66,84 +80,106 @@ router.route('')
  *                         description: Id for the user who will be assigned as manager for the created hotel
  *                         example: 603e02dfcc68d8751453b861
  */
-router.post('/addHotel/:userId', authorize(role.HotelManager), hotelController.addHotel);
+router.post(
+  "/addHotel/:userId",
+  authorize(role.HotelManager),
+  hotelController.addHotel
+);
 
 /**
-* @swagger
-* /hotels/:hotelid:
-*   post:
-*     security: 
-*       - bearerAuth: JWT
-*     summary: Add a room to a hotel
-*     description: Through a hotel id add a room to the hotel
-*     parameters: 
-*     - in: path
-*       name: hotelId
-*       required: true
-*       description: The unique ID of the hotel that the room will be added to
-*     responses:
-*       201:
-*         description: Hotel room was created succesfully.
-*         content:
-*           application/json:
-*             schema:
-*               type: object
-*               properties:
-*                 data:
-*                   type: array
-*                   items:
-*                     type: object
-*                     properties:
-*                        hotelid: ObjectId
-*                        description: The hotels ID
-*                        example: 603e02dfcc68d8751453b861
-*                     room:
-*                        type: room
-*                        description: A json object describing a room
-*                        example: {"roomNo": 8,"reservations":[{"dateStart": "2021-03-02T09:18:23.807+00:00","guestId": "Randi"}]}
-*/
-router.put('/:hotelId/user/:userId', authorize(role.HotelManager), hotelController.addRoomToHotel);
+ * @swagger
+ * /hotels/:hotelid:
+ *   post:
+ *     security:
+ *       - bearerAuth: JWT
+ *     summary: Add a room to a hotel
+ *     description: Through a hotel id add a room to the hotel
+ *     parameters:
+ *     - in: path
+ *       name: hotelId
+ *       required: true
+ *       description: The unique ID of the hotel that the room will be added to
+ *     responses:
+ *       201:
+ *         description: Hotel room was created succesfully.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                        hotelid: ObjectId
+ *                        description: The hotels ID
+ *                        example: 603e02dfcc68d8751453b861
+ *                     room:
+ *                        type: room
+ *                        description: A json object describing a room
+ *                        example: {"roomNo": 8,"reservations":[{"dateStart": "2021-03-02T09:18:23.807+00:00","guestId": "Randi"}]}
+ */
+router.put(
+  "/:hotelId/user/:userId",
+  authorize(role.HotelManager),
+  hotelController.addRoomToHotel
+);
 
 /**
-* @swagger
-* /hotels/AllHotelsWithRooms/:userId:
-*   get:
-*     security: 
-*      - bearerAuth: JWT
-*     summary: Returns hotelrooms for user
-*     description: Shows all hotelrooms where the given userID is found in the reservationlist
-*     parameters: 
-*     - in: path
-*       name: userId
-*       required: true
-*       description: The rooms that have this user ID in its reservationlist will be shown
-*     responses:
-*       200:
-*         description: Found hotelrooms
-*         content:
-*           application/json:
-*             schema:
-*               type: object
-*               properties:
-*                 data:
-*                   type: array
-*                   items:
-*                     type: object
-*                     properties:
-*                        hotelid: ObjectId
-*                        description: The hotels ID
-*                        example: 603e02dfcc68d8751453b861
-*                     room:
-*                        type: room
-*                        description: A json object describing a room
-*                        example: {"roomNo": 8,"reservations":[{"dateStart": "2021-03-02T09:18:23.807+00:00","guestId": "Randi"}]}
-*/
-router.get('/AllHotelsWithRooms/:userId', authorize([role.HotelManager, role.Admin, role.Guest]), hotelController.getHotelsWithRooms);
+ * @swagger
+ * /hotels/AllHotelsWithRooms/{userId}:
+ *   get:
+ *     security:
+ *      - bearerAuth: JWT
+ *     summary: Returns hotelrooms for user
+ *     description: Shows all hotelrooms where the given userID is found in the reservationlist
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The rooms that have this user ID in its reservationlist will be shown
+ *     responses:
+ *       200:
+ *         description: Found hotelrooms
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                        hotelid: ObjectId
+ *                        description: The hotels ID
+ *                        example: 603e02dfcc68d8751453b861
+ *                     room:
+ *                        type: room
+ *                        description: A json object describing a room
+ *                        example: {"roomNo": 8,"reservations":[{"dateStart": "2021-03-02T09:18:23.807+00:00","guestId": "Randi"}]}
+ */
+router.get(
+  "/AllHotelsWithRooms/:userId",
+  authorize([role.HotelManager, role.Admin, role.Guest]),
+  hotelController.getHotelsWithRooms
+);
 
 /*GET rooms from hotelid owned by manager*/
-router.get('/:userid/:hotelid', authorize(role.HotelManager), hotelController.getRoomsFromHotelID);
+router.get(
+  "/:userid/:hotelid",
+  authorize(role.HotelManager),
+  hotelController.getRoomsFromHotelID
+);
 
 /*GET available rooms*/
-router.get('/available/:userid/:hotelid', authorize(role.User), hotelController.getAvailableRoomsFromHotelid);
+router.get(
+  "/available/:userid/:hotelid",
+  authorize(role.User),
+  hotelController.getAvailableRoomsFromHotelid
+);
 
 module.exports = router;
